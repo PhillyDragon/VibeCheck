7cf0b86825bfa789c6197c5c3dfeaf05739d5821
PhillyDragon <154367349+PhillyDragon@users.noreply.github.com>
2023-12-19 14:34:17 -0600

    Update vibe.js

--- vibe.js
+++ vibe.js
@@ -1,7 +1,3 @@
-var precision = 100; 
-var randomnum = Math.floor(Math.random() * (10 * precision - 1 * precision) + 1 * precision) / (1*precision);
-
-
 function vibeCheck() {
   
 return "Random number is: ";
